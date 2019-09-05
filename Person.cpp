f3b1d9731f7d92f1801f73565d187acb2e845284
Peredo <peredo98@hotmail.com>
2019-09-05 13:26:25 -0500

    drivers and test

---
+++ Person.cpp
@@ -0,0 +1,15 @@
+
+#include<string> 
+using namespace std; 
+
+class Person{
+    public:
+        string name;
+        string lastname;
+        string id;
+        Person(string name, string lastName, string id){
+            
+
+        }
+    
+};
