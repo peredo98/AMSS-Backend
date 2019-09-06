5c790c13a55b54eee59dd6614167663c38c11408
Peredo <peredo98@hotmail.com>
2019-09-06 12:39:58 -0500

    json parser lib

--- sources/test.cpp
+++ sources/test.cpp
@@ -9,20 +9,29 @@
 
 #include "./../headers/FaceDB.h"
 
+#include "./../include/rapidjson/document.h"
+
 using namespace std; 
+
+using namespace rapidjson;
+
 int main(int, char**) {
     
     FaceDB db;
     // Crate Person createPerson(name, lastName, studentId, age, gender) eg:
     //db.createPerson("Ruben", "Salazar", "A01234567", 21, "Male");
 
-    //Get Person by StudentId getPersonById(studentId) eg:
-    //string str =  db.getPersonById("A01234567");
-    //printf("%s", str.c_str());
+    //Get Person's name by StudentId getPersonById(studentId) eg:
+    string json =  db.getPersonById("A01234567");
+    //printf("%s\n", json.c_str());
+    Document d;
+    d.Parse(json.c_str());
+    Value& val = d["name"];
+    printf("%s\n", val.GetString());
 
-    //Update imageURL by studentId
+    //Update imageURL by studentId insetImage(studentId, imageURL)
     //db.insertImage("A01234567", "/img/A01234567.png");
 
     //PrintDB
-    db.printDB();
+    //db.printDB();
 }
