e34bb6af1dfc247a0f5ef61cf5eb3a2f6d658b3d
Peredo <peredo98@hotmail.com>
2019-09-18 22:49:32 -0500

    Biometric data inputs

--- headers/FaceDB.h
+++ headers/FaceDB.h
@@ -45,13 +45,66 @@ class FaceDB{
             document << "name" << name << "lastName" << lastName << "studentId" << id << "age" << age  << "gender" << gender << "imageUrl" << imageURL;
             collection.insert_one(document.view());
         }
-        
-        void saveImage(Mat image, string fileName){
 
-            imwrite("./../img/" + fileName + ".png", image);
+         //to insert person in DB with photo and biometrics
+        void createPerson(string name, string lastName, string id, int age, string gender, vector<vector<int>> biometrics){
 
+            auto collection = conn["testdb"]["testcollection"];
+            
+            document << "name" << name << "lastName" << lastName << "studentId" << id << "age" << age  << "gender" << gender;
+            
+
+            auto array = document << "biometricData "<<  bsoncxx::builder::stream::open_array;
+
+            for (int i =0;i < biometrics.size(); i++) {
+
+                biometrics[i].size();
+                
+                array << bsoncxx::builder::stream::open_array;
+                
+                for (int j =0;j < biometrics[i].size(); j++) {
+                   array << biometrics[i][j];
+                }
+
+                array << bsoncxx::builder::stream::close_array;
+            }
+            array << bsoncxx::builder::stream::close_array;
+
+            collection.insert_one(document.view());
         }
 
+        //to insert person in DB with photo and biometrics
+        void createPerson(string name, string lastName, string id, int age, string gender, string imageURL, vector<vector<int>> biometrics){
+
+            auto collection = conn["testdb"]["testcollection"];
+            
+            document << "name" << name << "lastName" << lastName << "studentId" << id << "age" << age  << "gender" << gender << "imageUrl" << imageURL;
+            
+
+            auto array = document << "biometricData "<<  bsoncxx::builder::stream::open_array;
+
+            for (int i =0;i < biometrics.size(); i++) {
+
+                biometrics[i].size();
+                
+                array << bsoncxx::builder::stream::open_array;
+                
+                for (int j =0;j < biometrics[i].size(); j++) {
+                   array << biometrics[i][j];
+                }
+
+                array << bsoncxx::builder::stream::close_array;
+            }
+            array << bsoncxx::builder::stream::close_array;
+
+            collection.insert_one(document.view());
+        }
+        
+        void saveImage(Mat image, string fileName){
+            imwrite("./../img/" + fileName + ".png", image);
+        }
+
+        //write image url into existing user
         void insertImage(string id ,string fileName){
             auto collection = conn["testdb"]["testcollection"];
 
@@ -64,6 +117,39 @@ class FaceDB{
             string imageURL = ("./../img/" + fileName);
 
             update << "$set" << bsoncxx::builder::stream::open_document << "imageUrl" << imageURL << bsoncxx::builder::stream::close_document;
+
+            collection.update_one(filter.view(), update.view());
+        }
+        
+         //insert matrix with viometric data into existing user
+        void insertBiometric(string id, vector<vector<int>> biometrics){
+            auto collection = conn["testdb"]["testcollection"];
+
+            bsoncxx::builder::stream::document filter;
+
+            filter << "studentId" << id;
+
+            bsoncxx::builder::stream::document update;
+     
+            update << "$set" << bsoncxx::builder::stream::open_document;
+
+            auto array = update << "biometricData "<<  bsoncxx::builder::stream::open_array;
+
+            for (int i =0;i < biometrics.size(); i++) {
+
+                biometrics[i].size();
+                
+                array << bsoncxx::builder::stream::open_array;
+                
+                for (int j =0;j < biometrics[i].size(); j++) {
+                   array << biometrics[i][j];
+                }
+
+                array << bsoncxx::builder::stream::close_array;
+            }
+            array << bsoncxx::builder::stream::close_array;
+
+            update << bsoncxx::builder::stream::close_document;
 
             collection.update_one(filter.view(), update.view());
         }
--- sources/test.cpp
+++ sources/test.cpp
@@ -25,6 +25,18 @@ int main(int, char**) {
     // Crate Person createPerson(name, lastName, studentId, age, gender) eg:
     //db.createPerson("Ruben", "Salazar", "A01234567", 21, "Male");
 
+    // Create Person with imageURL createPerson(name, lastName, studentId, age, gender, imageURL) eg:
+    //db.createPerson("Ruben", "Salazar", "A01234567", 21, "Male", "A01234567.png");
+
+    // Create Person with biometrics createPerson(name, lastName, studentId, age, gender, biometricData) eg:
+    //vector<vector<int>> biometrics{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; ;
+    //db.createPerson("Ruben", "Salazar", "A01234567", 21, "Male", biometrics);
+
+    // Create Person with imageURL and BiometricData createPerson(name, lastName, studentId, age, gender, imageURL, biometricData) eg:
+    //vector<vector<int>> biometrics{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; ;
+    //db.createPerson("Ruben", "Salazar", "A01234567", 21, "Male", "A01234567.png", biometrics);
+
+
     /*
     //Get Person's name by StudentId getPersonById(studentId) eg:
     string json =  db.getPersonById("A01234567");
@@ -38,14 +50,24 @@ int main(int, char**) {
     */
 
     //Read image
+    /*
     Mat image;
     image = imread( "./../img/sample.jpeg", 1 );
+    */
 
     //Save image (the image will be saved with .png extension)
+    /*
     db.saveImage(image, "A01234567");
+    */
 
     //Update imageURL by studentId insetImage(studentId, fileName) | filename must include the file extension
+    /*
     db.insertImage("A01234567", "A01234567.png");
+    */
+
+    //insert matrix into an existing user
+    //vector<vector<int>> biometrics{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; ;
+    //db.insertBiometric("A01234567", biometrics);
 
     //PrintDB
     db.printDB();
